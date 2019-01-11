826e8fb4ff77be6cf7112b3605e75d49dcbdd1e1
vishi8 <46601502+vishi8@users.noreply.github.com>
2019-01-11 08:45:36 -0600

    Location added for first time.

--- Connect5Tori/src/Location.java
+++ Connect5Tori/src/Location.java
@@ -1,2 +1,28 @@
-public class Location {
+public class Location
+{
+    private int x, y, z;
+
+    public Location(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public String toString()
+    {
+        return "(x = " + x + ", y = " + y + ", z =" + z + ")";
+    }
 }
